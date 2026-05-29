b33b9e25bf54766d814c758d3e1fc718c7c325bb
Lee Hyeoun Beom <86977637+LHBM04@users.noreply.github.com>
2026-05-29 19:27:37 +0900

    Update Component.h

--- Homework #2(2026.05.15 ~ 2026.06.01)/Sources/Component.h
+++ Homework #2(2026.05.15 ~ 2026.06.01)/Sources/Component.h
@@ -22,6 +22,11 @@ public:
 	[[nodiscard]] GameObject* GetOwner() noexcept;
 	[[nodiscard]] const GameObject* GetOwner() const noexcept;
 
+	[[nodiscard]] bool IsStarted() const noexcept;
+	
+	[[nodiscard]] bool IsActive() const noexcept;
+	void Enable();
+
 protected:
 	virtual void OnAwake() {};
 	virtual void OnEnable() {};
@@ -47,11 +52,14 @@ protected:
 	virtual void OnDestroy() {};
 
 private:
+	void Awake();
+
 	void SetOwner(GameObject* owner_) noexcept;
 
 	GameObject* owner{ nullptr };
 
 	bool isStarted{ false };
+	bool isEnabled{ true };
 };
 
 inline GameObject* Component::GetOwner() noexcept
@@ -64,7 +72,47 @@ inline const GameObject* Component::GetOwner() const noexcept
 	return owner;
 }
 
+inline bool Component::IsStarted() const noexcept
+{
+	return isStarted;
+}
+
+inline bool Component::IsActive() const noexcept
+{
+	return isEnabled;
+}
+
+inline void Component::Awake()
+{
+
+}
+
+inline void Component::Enable()
+{
+
+}
+
 inline void Component::SetOwner(GameObject* owner_) noexcept
 {
 	owner = owner_;
+}
+
+inline void Scene::Update()
+{
+	if (!gameObject->IsActive())
+	{
+		return;
+	}
+
+	gameObject->Update(deltaTime_);
+}
+
+inline void GameObject::Update(float deltaTime_)
+{
+	if (!IsActive())
+	{
+		return;
+	}
+
+	// 컴포넌트 업데이트.
 }
