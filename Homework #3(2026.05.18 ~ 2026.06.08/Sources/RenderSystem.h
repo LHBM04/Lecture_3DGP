76d2d2f218166d434cc62a864d70722371f7a832
Lee Hyeoun Beom <86977637+LHBM04@users.noreply.github.com>
2026-06-02 00:00:45 +0900

    렌더 시스템 추가

---
+++ Homework #3(2026.05.18 ~ 2026.06.08/Sources/RenderSystem.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <cstddef>
+
+#include "Singleton.h"
+
+class RenderSystem : public Singleton<RenderSystem>
+{
+public:
+	static constexpr std::size_t BufferOffset{ 256 };
+	
+	struct alignas(BufferOffset) CameraConstants final
+	{
+
+	};
+
+	struct alignas(BufferOffset) LightConstants final
+	{
+
+	};
+
+	virtual ~RenderSystem() = default;
+
+	bool Initialize();
+	void Release();
+
+private:
+};
