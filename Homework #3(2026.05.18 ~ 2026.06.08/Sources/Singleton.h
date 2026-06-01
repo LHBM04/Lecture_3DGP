606b20b87f8a443885927a13ca0a82ba6bfda364
Lee Hyeoun Beom <86977637+LHBM04@users.noreply.github.com>
2026-06-02 00:00:39 +0900

    Create Singleton.h

---
+++ Homework #3(2026.05.18 ~ 2026.06.08/Sources/Singleton.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+template <class TInstance>
+class Singleton
+{
+public:
+	Singleton() = default;
+	virtual ~Singleton() = default;
+
+	// 복사 금지.
+	Singleton(const Singleton&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
+
+	// 이동 금지.
+	Singleton(Singleton&&) = delete;
+	Singleton& operator=(Singleton&&) = delete;
+
+	[[nodiscard]] static TInstance& GetInstance() noexcept;
+};
+
+template <class TInstance>
+TInstance& Singleton<TInstance>::GetInstance() noexcept
+{
+	static TInstance* instance{ new TInstance() };
+	return *instance;
+}
