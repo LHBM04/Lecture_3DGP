ae67b7eb9c2dcaf8f274f98be8668c07b178ce9c
Lee Hyeoun Beom <86977637+LHBM04@users.noreply.github.com>
2026-06-14 01:03:56 +0900

    Update Precompiled.h

--- Homework #4(2026.05.18 ~ 2026.06.17)/Sources/Precompiled.h
+++ Homework #4(2026.05.18 ~ 2026.06.17)/Sources/Precompiled.h
@@ -1,1 +1,13 @@
-#pragma once
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <windowsx.h>
+
+#include <wrl.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+
+#include <d3dcommon.h>
+#include <d3dcompiler.h>
