d9d1b1bb2327564f7428e8e58a72cf48a22104db
Lee Hyeoun Beom <86977637+LHBM04@users.noreply.github.com>
2026-06-14 01:04:00 +0900

    Update Main.cpp

---
+++ Homework #4(2026.05.18 ~ 2026.06.17)/Sources/Main.cpp
@@ -0,0 +1,11 @@
+﻿#include "Precompiled.h"
+
+INT APIENTRY wWinMain(
+	_In_ HINSTANCE hInstance, 
+	_In_opt_ HINSTANCE hPrevInstance, 
+	_In_ PWSTR lpCmdLine, 
+	_In_ INT nCmdShow)
+{
+	::MessageBoxW(nullptr, L"Hello, World!", L"Notice", NULL);
+	return 0;
+}
